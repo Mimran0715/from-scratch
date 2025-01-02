187b57422347f60d567924c3236f4b3dbd86e063
Mimran0715 <maleeha.imranm@gmail.com>
2025-01-02 10:11:46 -0800

    updating

---
+++ vision_transformer.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+import torch
+import torch.optim as optim
+import torch.utils.data as data
+import math
+import copy
+
+
+# taken from https://medium.com/thedeephub/building-vision-transformer-from-scratch-using-pytorch-an-image-worth-16x16-words-24db5f159e27
+
